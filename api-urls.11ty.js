28e23a190d04b972b484ded8849299ae903687ad
collin chen <njuclc@gmail.com>
2024-11-23 19:41:17 +0800

    feat: use function instead of class

--- api-urls.11ty.js
+++ api-urls.11ty.js
@@ -1,22 +1,20 @@
-const shortHash = require("short-hash");
+function ApiUrls() { }
 
-class ApiUrls {
-	data() {
-		return {
-			layout: false,
-			permalink: function(data) {
-				return `/api/urls.json`;
-			}
-		};
-	}
-
-	render(data) {
-		let resultData = {};
-		for(let urlData of data.urlsForApi) {
-			resultData[urlData.requestedUrl] = urlData;
+ApiUrls.prototype.data = function () {
+	return {
+		layout: false,
+		permalink: function (data) {
+			return `/api/urls.json`;
 		}
-		return JSON.stringify(resultData, null, 2);
+	};
+}
+
+ApiUrls.prototype.render = function (data) {
+	let resultData = {};
+	for (let urlData of data.urlsForApi) {
+		resultData[urlData.requestedUrl] = urlData;
 	}
+	return JSON.stringify(resultData, null, 2);
 }
 
 module.exports = ApiUrls;
